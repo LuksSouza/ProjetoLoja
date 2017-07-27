b07c234f5d6b76b8d8a52494af2d85e60f27a399
LuksSouza <lucas-machado@outlook.com>
2017-07-27 14:44:42 -0300

    Alterações na classe Produto para que, ao requisitar o formulário da página, o método formProduto da classe ProdutoController, retorne para o form via ModelAndView, uma lista de Marcas cadastrada no banco.
    Essa lista é exibida na JSP formProduto via ComboBox.
    
    A intenção é, ao submeter esse formulário, enviar a marca escolhida pelo usuário no cadastro do novo produto. Porém, alguns problemas estão ocorrendo.

--- src/main/java/br/com/projetoloja/controllers/ProdutoController.java
+++ src/main/java/br/com/projetoloja/controllers/ProdutoController.java
@@ -1,27 +1,42 @@
 package br.com.projetoloja.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import br.com.projetoloja.daos.MarcaDAO;
+import br.com.projetoloja.models.Marca;
+import br.com.projetoloja.models.Produto;
 
 @Controller
 public class ProdutoController {
 
+	@Autowired
+	MarcaDAO dao;
+	
 	@RequestMapping("/produtos/formProduto")
-	public String formProduto() {
+	public ModelAndView formProduto() {
 		System.out.println("Atendendo requisição da chamada '/produtos/formProduto'");
-		System.out.println("Retornando a view 'formProduto' para o container...");
+		System.out.println("Retornando a view 'formProduto' para o container com a lista de Marcas existente no banco...");
 		
-		return "produtos/formProduto";
+		List<Marca> marcas = dao.listar();
+		
+		ModelAndView view = new ModelAndView("produtos/formProduto");
+		view.addObject("marcas", marcas);
+		
+		return view;
 	}
 	
 	@RequestMapping("/cadastrarProduto")
-	public String gravar(String nome_produto, String preco, String marca) {
+	public String gravar(Produto produto) {
 		System.out.println("Atendendo requisição da chamada '/cadastrarProduto'");
 		System.out.println("Retornando a view 'sucesso' para o container...");
 		
-		System.out.println(nome_produto);
-		System.out.println(preco);
-		System.out.println(marca);
+		System.out.println(produto.getNome());
+		System.out.println(produto.getMarca().getDescricao());
 		
 		return "sucesso";
 	}
--- src/main/webapp/WEB-INF/views/produtos/formProduto.jsp
+++ src/main/webapp/WEB-INF/views/produtos/formProduto.jsp
@@ -1,6 +1,8 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
 	pageEncoding="UTF-8"%>
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<!-- Import da taglib -->
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<!DOCTYPE html>
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
@@ -11,13 +13,22 @@
 	<form action="/ProjetoLoja/cadastrarProduto" method="post">
 		<div>
 			<label>Nome do Produto:</label> <input type="text"
-				name="nome_produto" />
+				name="nome" />
 		</div>
 		<div>
 			<label>Preço:</label> <input type="text" name="preco" />
 		</div>
 		<div>
-			<label>Marca do Produto:</label> <input type="text" name="marca" />
+			<label>Marca do Produto:</label>
+			<!--<c:forEach items="${marcas}" var="marca">
+				<input type="hidden" value="${tipoPreco}" />
+				<td>${marca.descricao}</td>
+			</c:forEach>-->
+			<select>
+				<c:forEach var="marca" items="${marcas}">
+					<option value="${marca}" label="${marca.descricao}"/>
+				</c:forEach>
+			</select>
 		</div>
 		<button type="submit">Cadastrar</button>
 	</form>
